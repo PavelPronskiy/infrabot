4d55889bede8bb7d584bc194216d4225831daea1
Pavel Pronskiy <pavel@pronskiy.ru>
2018-05-11 23:37:34 +0300

    Infrabot tests camshot

--- modules/camshot.js
+++ modules/camshot.js
@@ -26,7 +26,8 @@ module.exports = {
 		bot.on(['/camshot'], function(msg) {
 			// let hostname = execSync('hostname');
 			// let message = '*' + 'report ' + hostname + '*' + '```' + uptime + '```';
-			var message = INFRABOT_PLUGINS;
+			// var message = INFRABOT_PLUGINS;
+			var message = '31231';
 
 			/*childProcess.exec('bash ./setup.sh status', function (error, stdout, stderr) {
 				var message = '``` ' + stdout + ' ```';
